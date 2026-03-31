64a4a2cd9b43c25c579fb9ba39cf3d5fc3248b7c
Nkwenti Deshnic <nkwentideshnic@gmail.com>
2026-03-31 16:39:18 +0100

    feat: enable TypeScript and ESLint error ignoring during production builds

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,18 @@ const nextConfig = {
         hostname: "images.unsplash.com"
       }
     ]
-  }
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete 
+    // even if your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete 
+    // even if your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
